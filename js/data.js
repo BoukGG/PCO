e0c417cfffc4b5289a94f63e23895622ca9da95c
BoukGG <ggboukgg@gmail.com>
2026-09-07 21:01:35 -0400

    Update steps count in PCO_DATA

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 window.PCO_DATA = {
   raised: 0,
   goal: 10000,
-  steps: "1,342,908",
+  steps: "3,096,936",
   charityLine: "Tax-deductible donations are processed by [charity], a registered 501(c)(3), EIN [EIN].",
   venmo: "@[BlakeAnderson3]",
   email: "[contact email]",
